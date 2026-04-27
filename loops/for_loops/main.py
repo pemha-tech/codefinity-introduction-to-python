bfaa124021c7eea68d284d1c3dd4fef46852e6f2
pemha-tech <>
2026-04-27 20:03:16 +0000

    For loops

---
+++ loops/for_loops/main.py
@@ -0,0 +1,6 @@
+prices = [12.99, 8.50, 15.75, 23.00, 7.25]
+total = 0
+# Write your code here
+for price in prices:
+    total = price + total
+print("the final total after the loop completes",total)
