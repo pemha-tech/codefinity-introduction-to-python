b8c609ca663d589fa2348d09dc2d4f6efebe16f0
pemha-tech <>
2026-04-29 23:36:28 +0000

    Range Function

---
+++ loops/range_function/main.py
@@ -0,0 +1,5 @@
+# List of products on promotion for each weekday
+daily_promotions = ["Milk", "Eggs", "Bread", "Apples", "Oranges"]
+
+# List of weekdays corresponding to the promotions
+weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
