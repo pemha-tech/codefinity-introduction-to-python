8cc4ba1c3b4b5d66192b0cbb315e78517d50c352
pemha-tech <>
2026-04-30 15:44:49 +0000

    Range Function

--- loops/range_function/main.py
+++ loops/range_function/main.py
@@ -3,3 +3,7 @@ daily_promotions = ["Milk", "Eggs", "Bread", "Apples", "Oranges"]
 
 # List of weekdays corresponding to the promotions
 weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+for day in range(5):
+    promotion = daily_promotions[day]
+    weekday = weekdays[day]
+    print(f"{weekday}: promotion on {promotion}")
